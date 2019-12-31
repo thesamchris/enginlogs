205558574afec607dd7b9d3b74fbb9a99ba253a2
Samuel Paul Christopher <thesamchris@gmail.com>
2019-12-31 17:41:12 +0800

    completed add item form

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -7,20 +7,8 @@ function App() {
   return (
     <div className="App">
       <AddInitial />
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>show items</div>
+      <div>show bookings</div>
     </div>
   );
 }
--- src/components/addInitial/form.js
+++ src/components/addInitial/form.js
@@ -3,25 +3,36 @@ import React from 'react'
 class AddIntialForm extends React.Component {
     constructor() {
         super()
+        this.state = {
+            itemName: '',
+            itemQuantity: ''
+        }
         this.addNewItem = this.addNewItem.bind(this)
+        this.updateState = this.updateState.bind(this)
     }
 
-    addNewItem(e, arg) {
+    addNewItem(e) {
         e.preventDefault()
         let newItemRef = this.props.initialRef.push()
         let newItem = {
-            name: 'Blue Plastic Balls',
-            quantity: 68
+            name: this.state.itemName,
+            quantity: this.state.itemQuantity
         }
        newItemRef.set({...newItem})
     }
 
+    updateState(e) {
+        let { id, value } = e.target
+        this.setState({
+            [id]: value
+        })
+    }
+
     render() {
-        console.log(this)
         return (
             <div>
-                <input placeholder="Name" />
-                <input placeholder="Quantity" />
+                <input id="itemName" onChange={this.updateState} placeholder="Name" value={this.state.itemName}/>
+                <input id="itemQuantity" placeholder="Quantity" type="number" onChange={this.updateState} value={this.state.itemQuantity}/>
                 <button onClick={this.addNewItem}>add item!</button>
             </div>
         )
