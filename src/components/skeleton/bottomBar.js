be2b98b4931daf188f327af2ff5d293d687b17c2
Samuel Paul Christopher <thesamchris@gmail.com>
2020-07-13 23:33:09 +0800

    active marker for bottom bar

--- src/components/skeleton/bottomBar.js
+++ src/components/skeleton/bottomBar.js
@@ -1,24 +1,31 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 import './bottomBar.css'
 
 const BottomBar = () => (
-    <div className="bar__container">
-        <div className="bar__sections_container">
-            <Link to="/items" className="bar__section">
-                <div className="bar__section_image bar__items"></div>
-                <div className="bar__section_name">items</div>
-            </Link>
-            <Link to="/dashboard" className="bar__section">
-                <div className="bar__section_image bar__dashboard"></div>
-                <div className="bar__section_name">dashboard</div>
-            </Link>
-            <Link to="/loan" className="bar__section">
-                <div className="bar__section_image bar__loan"></div>
-                <div className="bar__section_name">loan</div>
-            </Link>
-        </div>
-    </div>
+	<div className="bar__container">
+		<div className="bar__sections_container">
+			<NavLink activeClassName="active" to="/items" className="bar__section">
+				<div className="bar__section_image bar__items"></div>
+				<div className="bar__section_name">items</div>
+				<div className="bar__marker"></div>
+			</NavLink>
+			<NavLink
+				activeClassName="active"
+				to="/dashboard"
+				className="bar__section"
+			>
+				<div className="bar__section_image bar__dashboard"></div>
+				<div className="bar__section_name">dashboard</div>
+				<div className="bar__marker"></div>
+			</NavLink>
+			<NavLink activeClassName="active" to="/loan" className="bar__section">
+				<div className="bar__section_image bar__loan"></div>
+				<div className="bar__section_name">loan</div>
+				<div className="bar__marker"></div>
+			</NavLink>
+		</div>
+	</div>
 )
 
 export default BottomBar
